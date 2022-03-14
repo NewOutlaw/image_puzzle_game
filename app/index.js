bfb78e8260c2bad305774abb0ebc418e27439fce
Andi <andreas.schindlbeck@mail.de>
2022-03-14 19:43:29 +0100

    docker support

--- app/index.js
+++ app/index.js
@@ -5,6 +5,30 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+console.log("hello world");
+
+//SETS MODE EITHER LIVE OR LOCAL
+var live_or_local = process.env.LIVE_OR_LOCAL; //SET IN DOCKERFILE
+
+var live_url = process.env.URL_LIVE;
+var live_port = parseInt(process.env.PORT_LIVE);
+console.log(live_port);
+
+var local_url = process.env.URL_LOCAL;
+var local_port = parseInt(process.env.PORT_LOCAL);
+
+var isLive = false;
+var serverUrl = local_url + local_port;
+var port = local_port;
+
+if(live_or_local == 'live')
+{
+    isLive = true;
+    serverUrl = live_url;
+    port = live_port;
+}
+
+
 const publicDirectoryPath = path.join(__dirname, '/public')
 app.use(express.static(publicDirectoryPath))
 
@@ -12,9 +36,9 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile('/index.html');
 })
 
-app.listen(3000, () => {
-    console.log("server listening");
+app.listen(port, () => {
+    console.log(live_or_local + " environment started at port " + port);
 });
